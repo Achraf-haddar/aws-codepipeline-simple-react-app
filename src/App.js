09a807bc59b31bcb348f60fd8247aaf6ef055195
Achraf-haddar <62722610+Achraf-haddar@users.noreply.github.com>
2021-12-19 12:05:40 +0200

    update

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
+        <p id="Phrase">
           Hello World React Application !
         </p>
       </header>
