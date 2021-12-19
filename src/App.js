617156762c113baad31fc8f4b62fa4936cd793fa
Achraf-haddar <62722610+Achraf-haddar@users.noreply.github.com>
2021-12-19 14:11:55 +0200

    update

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p id="Phrase">
-          Hello World React Application !
+          Hello World: Simple React Application !
         </p>
       </header>
     </div>
